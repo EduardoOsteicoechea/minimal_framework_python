aaca79b74f3b552e033228842be37b178ce3c5ca
eduar <eduar@EDUARDO>
2025-08-28 14:58:39 -0400

    update

---
+++ app.py
@@ -0,0 +1,15 @@
+# app.py
+
+def application(environ, start_response):
+    """
+    A bare-bones WSGI application.
+    """
+    status = '200 OK'
+    headers = [('Content-type', 'text/plain')]
+
+    start_response(status, headers)
+
+    # The body of the response, returned as an iterable of byte strings.
+    response_body = "Change from local"
+    
+    return [response_body.encode('utf-8')]
