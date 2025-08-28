3c2b0d2841297934ce33e842dcab3b23b109bd06
eduar <eduar@EDUARDO>
2025-08-28 15:29:25 -0400

    update

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ def application(environ, start_response):
     A bare-bones WSGI application.
     """
     status = '200 OK'
-    headers = [('Content-type', 'text/plain')]
+    headers = [('Content-type', 'text/html')]
 
     start_response(status, headers)
 
-    # The body of the response, returned as an iterable of byte strings.
     response_body = """
 
 <!DOCTYPE html>
