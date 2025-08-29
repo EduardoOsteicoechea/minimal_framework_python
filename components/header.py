76454e4e60bc97673a3bd749f2866be31a1c98f9
eduar <eduar@EDUARDO>
2025-08-29 07:19:15 -0400

    update

--- components/header.py
+++ components/header.py
@@ -1,12 +1,12 @@
 # components/header.py
 
 class PageHeader:
-  def __init__(self):
-    pass
-  
-  def html(self):
-    return """
-    <div>
-      This is the header
-    </div>
-"""
+    def __init__(self):
+        pass
+
+    def html(self):
+        return """
+            <div>
+              This is the header
+            </div>
+        """
--- pages/home.py
+++ pages/home.py
@@ -3,6 +3,7 @@
 from pages.base import PageBase
 from components.header import PageHeader
 
+
 class Page(PageBase):
     def __init__(self):
         super().__init__()
