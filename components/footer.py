c3cea006fd18f4649e65a8453c903ed1bcebc369
eduar <eduar@EDUARDO>
2025-08-29 16:38:20 -0400

    update

--- components/footer.py
+++ components/footer.py
@@ -5,6 +5,6 @@ from .base import ComponentBase
 
 class PageFooter(ComponentBase):
     def __init__(self):
-        super.__init__(HTMLContaining.DIV, "This is the footer",["footer"])
+        super().__init__(HTMLContaining.DIV, "This is the footer",["footer"])
         self.css_file_names.extend(["footer"])
         self.js_file_names.extend(["footer"])
