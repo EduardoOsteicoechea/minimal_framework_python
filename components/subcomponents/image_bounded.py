97da06e1e7fb1e85b530af45129d19c006eefab2
eduar <eduar@EDUARDO>
2025-08-30 10:06:07 -0400

    update

--- components/subcomponents/image_bounded.py
+++ components/subcomponents/image_bounded.py
@@ -18,7 +18,7 @@ class ImageBounded(ComponentBase):
                 #"src":"static/images/personal_photo_white_head_600x600.jpg", 
                 "width":"300%",
                 "alt":"",
-                "style":"right:25px;top:-10px"
+                "style":"right:35px;top:-10px"
             },
             "this is a parag in the header",
         ))
