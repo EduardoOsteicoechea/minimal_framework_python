3ef7596697aecb13191570abfd2dc4bec9070511
eduar <eduar@EDUARDO>
2025-08-30 10:44:27 -0400

    update

--- pages/home.py
+++ pages/home.py
@@ -2,7 +2,9 @@
 
 from pages.base import PageBase
 from components.header import PageHeader
-from components.article_001 import PageArticle001
+# from components.article_001 import PageArticle001
+from components.subcomponents.article_title_001 import ArticleTitle001
+from components.subcomponents.article_body_001 import ArticleBody001
 
 
 class HomePage(PageBase):
@@ -10,4 +12,8 @@ class HomePage(PageBase):
         super().__init__()
         self.set_title("eduardoos")
         self.addComponent(PageHeader())
-        self.addComponent(PageArticle001())
+        # self.addComponent(PageArticle001())
+        self.css_file_names.extend(["article_001"])
+        self.js_file_names.extend(["article_001"])
+        self.addComponent(self,ArticleTitle001())
+        self.addComponent(self,ArticleBody001())
