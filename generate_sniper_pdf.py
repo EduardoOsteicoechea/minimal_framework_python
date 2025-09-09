d06be6813a9095dcb56e3e52ea10dd4e4441f3b3
eduar <eduar@EDUARDO>
2025-09-09 17:02:48 -0400

    update

--- generate_sniper_pdf.py
+++ generate_sniper_pdf.py
@@ -6,20 +6,41 @@ from reportlab.lib.pagesizes import letter
 class VehicleRegistrationCertificate:
     def __init__(self, data: dict):
         self.nombre_de_la_empresa = data.get("nombre_de_la_empresa")
-        self.fecha_emision = data.get("fecha_emision")
-        self.factura_1_n_fecha = data.get("factura_1_n_fecha")
+        self.fecha_de_emision = data.get("fecha_de_emision")
+        self.serie_de_numero_de_factura_1 = data.get("serie_de_numero_de_factura_1")
+        self.numero_de_factura_1 = data.get("numero_de_factura_1")
+        self.fecha_de_factura_1 = data.get("fecha_de_factura_1")
         self.placa = data.get("placa")
         self.marca = data.get("marca")
-        self.modelos = data.get("modelos")
+        self.modelo = data.get("modelo")
         self.ano_de_fabricacion = data.get("ano_de_fabricacion")
-        
+        self.serial_niv = data.get("serial_niv")
+        self.ano_modelo = data.get("ano_modelo")
+        self.serial_chasis = data.get("serial_chasis")
+        self.serial_motor = data.get("serial_motor")
+        self.serial_carrocería = data.get("serial_carrocería")
+        self.serial_carrocería = data.get("serial_carrocería")
+        self.clase = data.get("clase")
+        self.tipo = data.get("tipo")
+        self.uso = data.get("uso")
+        self.servicio = data.get("servicio")
+        self.color_pr = data.get("color_pr")
+        self.color_sec = data.get("color_sec")
+        self.n_puestos = data.get("n_puestos")
+        self.n_ejes = data.get("n_ejes")
+        self.peso_tara = data.get("peso_tara")
+        self.cap_de_carga = data.get("cap_de_carga")
+        self.puerto_de_entrada = data.get("puerto_de_entrada")
+        self.planilla_liq_grv_n = data.get("planilla_liq_grv_n")
+        self.planilla_liq_grv_fecha = data.get("planilla_liq_grv_fecha")
+        self.factura_de_adquisicion_n = data.get("factura_de_adquisicion_n")
+        self.factura_de_adquisicion_fecha = data.get("factura_de_adquisicion_fecha")
+        self.refeciv = data.get("refeciv")
+        self.homologacion_n = data.get("homologacion_n")
+        self.homologacion_fecha = data.get("homologacion_fecha")
+        self.fecha_fin_convenio = data.get("fecha_fin_convenio")
+                
         self.fields = list(vars(self).items())
-
-        # self.fields = [
-        #     ["nombre_de_la_empresa", self.nombre_de_la_empresa],
-        #     ["fecha_emision", self.fecha_emision],
-        #     ["factura_1_n_fecha", self.factura_1_n_fecha],
-        # ]
 
 def generate_sniper_pdf(decoded_body: str) -> bytes:
     buffer = BytesIO()
